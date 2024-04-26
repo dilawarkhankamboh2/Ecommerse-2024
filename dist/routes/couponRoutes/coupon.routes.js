78b505e2f56ce718db6cdeb57b8a4a4a4bfff07d
dilawarkhankamboh2 <dilawarkhankamboh17@gmail.com>
2024-04-26 08:46:00 -0700

    add all coupons controller in coupon routes

--- dist/routes/couponRoutes/coupon.routes.js
+++ dist/routes/couponRoutes/coupon.routes.js
@@ -1,11 +1,13 @@
+import { deleteComment } from "../../controllers/comment/commentControler.js";
+import { allCoupon, applyDiscount, createCoupon } from "../../controllers/coupon/couponControler.js";
 // coupon routes
 export const couponRoutes = (app) => {
     // create coupon
-    app.post("/api/coupon/create");
+    app.post("/api/coupon/create", createCoupon);
     // apply coupon discount
-    app.get("/api/coupon/discount");
+    app.get("/api/coupon/discount", applyDiscount);
     // get all coupons
-    app.get("/api/coupon/all");
+    app.get("/api/coupon/all", allCoupon);
     // delete coupon
-    app.delete("/api/coupon/delete");
+    app.delete("/api/coupon/delete", deleteComment);
 };
